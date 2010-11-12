52d95fe9fe10f8b0a1b15a69ef22e9a34a38d183
Ben Jackson <ben.jackson1@gmail.com>
2010-11-12 06:35:01 -0500

    Added the created_at timestamp

--- lib/devise/orm/neo4j/hook.rb
+++ lib/devise/orm/neo4j/hook.rb
@@ -6,6 +6,9 @@ module Devise
           extend Schema
           devise_modules.each { |m| send(m) if respond_to?(m, true) }
           yield
+          
+          # ensure we have the created_at property
+          property :created_at, :type => Time
         end
       end
 		end
