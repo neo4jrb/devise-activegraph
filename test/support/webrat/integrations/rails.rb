46ab9ca2d83377d8e3285977e620c552b33247a4
Ben Jackson <ben.jackson1@gmail.com>
2011-05-24 13:17:34 +0100

    No longer needed

--- test/support/webrat/integrations/rails.rb
+++
@@ -1,9 +0,0 @@
-require 'webrat/core/elements/form'
-
-module Webrat
-  class Form
-    def self.parse_rails_request_params(params)
-      Rack::Utils.parse_nested_query(params)
-    end
-  end
-end
