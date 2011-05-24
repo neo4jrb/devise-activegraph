3e5a251c79f3c2afb10fa644985d551e4d93f3a0
Ben Jackson <ben.jackson1@gmail.com>
2011-05-24 17:20:44 +0100

    Added tests for the generator

---
+++ test/generators/neo4j/devise_generator_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+require 'tmpdir'
+require 'rails/generators/test_case'
+require 'generators/neo4j/devise_generator.rb'
+
+class Neo4j::Generators::ModelGeneratorTest < Rails::Generators::TestCase
+  tests Neo4j::Generators::DeviseGenerator
+  destination File.join(Dir.tmpdir, File.dirname(__FILE__))
+  setup :prepare_destination
+
+  def assert_class(klass, content)
+    assert content =~ /class #{klass}(\(.+\))?(.*?)\nend/m, "Expected to have class #{klass}"
+    yield $2.strip if block_given?
+  end
+  
+  test "invoke with model name" do
+    content = run_generator %w(Player)
+
+    assert_file "app/models/player.rb" do |player|
+      assert_class "Player", player do |klass|
+        assert_match /Neo4j::Rails::Model/, klass
+        assert_match /attr_accessible :email, :password, :password_confirmation, :remember_me/, klass        
+      end
+    end
+  end
+  
+end
