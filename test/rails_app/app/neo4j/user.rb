c4acf5128852e6b2fcc3990e24f0768945d0bb4f
Brian Underwood <brian@brian-underwood.codes>
2016-12-19 21:50:19 -0500

    Fix test model

--- test/rails_app/app/neo4j/user.rb
+++ test/rails_app/app/neo4j/user.rb
@@ -1,47 +1,63 @@
 require 'shared_user'
 
+
 class User
   include Neo4j::ActiveNode
   include SharedUser
 
+  #
+  # Neo4j.rb needs to have property definitions before any validations. So, the property block needs to come before
+  # loading your devise modules.
+  #
+  # If you add another devise module (such as :lockable, :confirmable, or :token_authenticatable), be sure to
+  # uncomment the property definitions for those modules. Otherwise, the unused property definitions can be deleted.
+  #
 
-  property :username,       type: String
-  property :facebook_token, type: String, index: :exact
-#  property :id
+  property :created_at, type: DateTime
+  property :updated_at, type: DateTime
 
   ## Database authenticatable
-  property :email,              type: String, default: '', null: false, index: :exact
-  property :encrypted_password, type: String, default: ''
-  
+  property :email, type: String, default: ''
+  validates :email, presence: true
+
+  property :encrypted_password
+
+  ## If you include devise modules, uncomment the properties below.
+
   ## Recoverable
-  property :reset_password_token,   type: String, index: :exact
+  property :reset_password_token
   property :reset_password_sent_at, type: DateTime
 
   ## Rememberable
   property :remember_created_at, type: DateTime
 
   ## Trackable
-  property :sign_in_count,      type: Integer, default: 0
+  property :sign_in_count, type: Integer, default: 0
+  validates :sign_in_count, presence: true
   property :current_sign_in_at, type: DateTime
-  property :last_sign_in_at,    type: DateTime
-  property :current_sign_in_ip, type: String
-  property :last_sign_in_ip,    type: String
+  property :last_sign_in_at, type: DateTime
+  property :current_sign_in_ip, type:  String
+  property :last_sign_in_ip, type: String
 
   ## Confirmable
-  property :confirmation_token,   type: String, index: :exact
-  property :confirmed_at,         type: DateTime
-  property :confirmation_sent_at, type: DateTime
-#  property :unconfirmed_email,   type: String  # Only if using reconfirmable
+  # property :confirmation_token
+  # property :confirmed_at, type: DateTime
+  # property :confirmation_sent_at, type: DateTime
+  # property :unconfirmed_email # Only if using reconfirmable
 
   ## Lockable
-  property :failed_attempts, type: Integer, default: 0
-  property :unlock_token,    type: String, index: :exact
-  property :locked_at,       type: DateTime
+  # property :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
+  # validates :failed_attempts, presence: true
+  # property :unlock_token, type: String # Only if unlock strategy is :email or :both
+  # property :locked_at, type: DateTime
 
-  property :created_at, type: DateTime
-  property :updated_at, type: DateTime
+  ## Token authenticatable
+  # property :authentication_token, type: String
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
 
 end
-
-UserAdapter  = User.to_adapter unless User.is_a?(OrmAdapter::Base)
-
