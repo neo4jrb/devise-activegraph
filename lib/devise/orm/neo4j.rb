10335d56041a8679ff43017d38039d1570046f06
amitsuryavanshi <amitbsuryavanshi@gmail.com>
2020-03-04 16:05:05 +0530

    updating to activegraph

--- lib/devise/orm/neo4j.rb
+++ lib/devise/orm/neo4j.rb
@@ -2,5 +2,5 @@ require 'devise-neo4j'
 require 'devise/orm/neo4j/date_time'
 require 'devise/orm/neo4j/nil_class_converter'
 
-Neo4j::ActiveNode::ClassMethods.send :include, Devise::Models
+ActiveGraph::Node::ClassMethods.send :include, Devise::Models
 
